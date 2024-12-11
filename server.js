a530a04e79e7eed6707adb9ce24891674056ba8e
Damien Ticer <damienticer+git@gmail.com>
2024-12-10 20:50:29 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config(); //Ensure environment variables are loaded
 const mysql = require("mysql2/promise");
 const express = require("express");
 const bcrypt = require("bcrypt");
@@ -27,8 +27,9 @@ app.options("*", cors());
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
